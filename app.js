42cbf8e8d408e20e4fb70899468fdfce6ea97373
Anas-Kaekaty <aanaass5432760@gmail.com>
2025-04-06 01:16:00 +0300

    add 404.html and fix home page bug

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ window.navigateTo = navigateTo;
 // تهيئة التطبيق عند التحميل
 window.addEventListener('DOMContentLoaded', () => {
     const userRole = 'seller'; // افتراضيًا (يمكن جلبها من localStorage)
+    const redirect = sessionStorage.redirect;// إعادة التوجيه إلى المسار المحفوظ بعد تحميل الصفحة
+    if (redirect) {
+    sessionStorage.removeItem('redirect');
+    window.history.replaceState({}, '', redirect);
     renderNavbar(userRole);
     handleRoute();
-    
+    }
 });
 
 // تحديث الصفحة عند استخدام زر الرجوع/التقدم
